ee94db962e80ffa31e7b78641d4a1bd1266c1fb3
imoebere <imoebereedward2017@gmail.com>
2025-01-31 16:32:29 +0100

    SharedServiceContainer

--- E-Commerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
+++ E-Commerce.SharedLibrary/DependencyInjection/SharedServiceContainer.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using E_Commerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
+using Serilog;
 
 namespace E_Commerce.SharedLibrary.DependencyInjection
 {
@@ -10,7 +14,37 @@ namespace E_Commerce.SharedLibrary.DependencyInjection
            IConfiguration config, string fileName) where TContext : DbContext
         {
             // Add Generic Database context
+            services.AddDbContext<TContext>(options => options.UseSqlServer(
+                config.GetConnectionString("eCommerceConnection"),
+                sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()));
+
+            // Configure serialog logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{fileName}-.text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+
+            // Add JWT Authentication Scheme
+            AuthenticationSheme.AddAuthenticationScheme(services, config);
             return services;
+        }
+
+        public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+        {
+            // Use global Exception
+            app.UseMiddleware<GlobalException>();
+
+            // Register middleware to block outside Api Calls
+            app.UseMiddleware<ListenToApiGateway>();
+            return app;
+
+
         }
     }
 }
